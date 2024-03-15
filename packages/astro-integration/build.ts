600e40ebf0bccb35328c6628b276b994738a8024
Shinya Fujino <shf0811@gmail.com>
2024-03-16 00:07:59 +0900

    Bun!

--- packages/astro-integration/build.ts
+++
@@ -1,8 +0,0 @@
-// biome-ignore lint/correctness/noUndeclaredVariables: <explanation>
-await Bun.build({
-	entrypoints: ["./src/index.ts"],
-	outdir: "./dist",
-	target: "node",
-	format: "esm",
-	sourcemap: "external",
-});
